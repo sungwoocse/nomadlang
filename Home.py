84ec1a259b943e941f49b9465121fbffbbf22bf7
sungwoocse <159869978+sungwoocse@users.noreply.github.com>
2024-08-07 13:58:26 +0900

    test

--- Home.py
+++ Home.py
@@ -1,11 +1,23 @@
 import streamlit as st
 
-st.title("Hello world!")
-
-st.subheader("Welcome to Streamlit!")
+st.set_page_config(
+    page_title="FullstackGPT Home",
+    page_icon="🤖",
+)
 
 st.markdown(
     """
-    #### I love it!
+# Hello!
+            
+Welcome to my FullstackGPT Portfolio!
+            
+Here are the apps I made:
+            
+- [ ] [DocumentGPT](/DocumentGPT)
+- [ ] [PrivateGPT](/PrivateGPT)
+- [ ] [QuizGPT](/QuizGPT)
+- [ ] [SiteGPT](/SiteGPT)
+- [ ] [MeetingGPT](/MeetingGPT)
+- [ ] [InvestorGPT](/InvestorGPT)
 """
 )
