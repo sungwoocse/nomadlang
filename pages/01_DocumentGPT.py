e0abef8efe22417f7090fb55e19b0e495550600a
sungwoocse <159869978+sungwoocse@users.noreply.github.com>
2024-08-07 14:33:09 +0900

    7.7

--- pages/01_DocumentGPT.py
+++ pages/01_DocumentGPT.py
@@ -1,33 +1,35 @@
-import os
-import streamlit as st
+from langchain.prompts import ChatPromptTemplate
 from langchain.document_loaders import UnstructuredFileLoader
 from langchain.embeddings import CacheBackedEmbeddings, OpenAIEmbeddings
+from langchain.schema.runnable import RunnableLambda, RunnablePassthrough
 from langchain.storage import LocalFileStore
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.vectorstores.faiss import FAISS
+from langchain.chat_models import ChatOpenAI
+import streamlit as st
 
-# 프로젝트 루트 디렉토리 경로 설정
-ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+st.set_page_config(
+    page_title="DocumentGPT",
+    page_icon="📃",
+)
 
+llm = ChatOpenAI(
+    temperature=0.1,
+)
+
+
+@st.cache_data(show_spinner="Embedding file...")
 def embed_file(file):
     file_content = file.read()
-    cache_dir = os.path.join(ROOT_DIR, ".cache", "files")
-    os.makedirs(cache_dir, exist_ok=True)
-    file_path = os.path.join(cache_dir, file.name)
-    
+    file_path = f"./.cache/files/{file.name}"
     with open(file_path, "wb") as f:
         f.write(file_content)
-    
-    embeddings_dir = os.path.join(ROOT_DIR, ".cache", "embeddings", file.name)
-    os.makedirs(embeddings_dir, exist_ok=True)
-    cache_dir = LocalFileStore(embeddings_dir)
-    
+    cache_dir = LocalFileStore(f"./.cache/embeddings/{file.name}")
     splitter = CharacterTextSplitter.from_tiktoken_encoder(
         separator="\n",
         chunk_size=600,
         chunk_overlap=100,
     )
-    
     loader = UnstructuredFileLoader(file_path)
     docs = loader.load_and_split(text_splitter=splitter)
     embeddings = OpenAIEmbeddings()
@@ -37,6 +39,41 @@ def embed_file(file):
     return retriever
 
 
+def send_message(message, role, save=True):
+    with st.chat_message(role):
+        st.markdown(message)
+    if save:
+        st.session_state["messages"].append({"message": message, "role": role})
+
+
+def paint_history():
+    for message in st.session_state["messages"]:
+        send_message(
+            message["message"],
+            message["role"],
+            save=False,
+        )
+
+
+def format_docs(docs):
+    return "\n\n".join(document.page_content for document in docs)
+
+
+prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            """
+            Answer the question using ONLY the following context. If you don't know the answer just say you don't know. DON'T make anything up.
+            
+            Context: {context}
+            """,
+        ),
+        ("human", "{question}"),
+    ]
+)
+
+
 st.title("DocumentGPT")
 
 st.markdown(
@@ -44,15 +81,34 @@ st.markdown(
 Welcome!
             
 Use this chatbot to ask questions to an AI about your files!
+
+Upload your files on the sidebar.
 """
 )
 
-file = st.file_uploader(
-    "Upload a .txt .pdf or .docx file",
-    type=["pdf", "txt", "docx"],
-)
+with st.sidebar:
+    file = st.file_uploader(
+        "Upload a .txt .pdf or .docx file",
+        type=["pdf", "txt", "docx"],
+    )
 
 if file:
     retriever = embed_file(file)
-    s = retriever.invoke("winston")
-    s
+    send_message("I'm ready! Ask away!", "ai", save=False)
+    paint_history()
+    message = st.chat_input("Ask anything about your file...")
+    if message:
+        send_message(message, "human")
+        chain = (
+            {
+                "context": retriever | RunnableLambda(format_docs),
+                "question": RunnablePassthrough(),
+            }
+            | prompt
+            | llm
+        )
+        response = chain.invoke(message)
+        send_message(response.content, "ai")
+
+else:
+    st.session_state["messages"] = []
